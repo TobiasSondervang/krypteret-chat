bd42a8864fe1fe52c57cd4dcdb6cd0de9b3fbab0
TobiasSondervang <tobiassondervangm@gmail.com>
2025-04-25 11:19:04 +0200

    Update setupIndexes.js

--- setupIndexes.js
+++ setupIndexes.js
@@ -1,20 +1,21 @@
 const { MongoClient } = require('mongodb');
 
-async function setupIndexes() {
-  const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-  const client = new MongoClient(uri);
+  async function setupIndexes() {
+    const uri = 'mongodb+srv://tobias:2006Tobias@cluster0.jwp1omu.mongodb.net/chatdb?retryWrites=true&w=majority&appName=Cluster0';
+    const client = new MongoClient(uri);
 
-  try {
-    await client.connect();
-    const db = client.db('chatdb');
-    await db.collection('messages').createIndex({ sender: 1, folder: 1 });
-    await db.collection('messages').createIndex({ recipient: 1, folder: 1 });
-    console.log('Indekser oprettet succesfuldt');
-  } catch (error) {
-    console.error('Fejl ved oprettelse af indekser:', error);
-  } finally {
-    await client.close();
+    try {
+      await client.connect();
+      console.log('Connected to MongoDB');
+      const db = client.db('chatdb');
+      await db.collection('messages').createIndex({ sender: 1, folder: 1 });
+      await db.collection('messages').createIndex({ recipient: 1, folder: 1 });
+      console.log('Indekser oprettet succesfuldt');
+    } catch (error) {
+      console.error('Fejl ved oprettelse af indekser:', error);
+    } finally {
+      await client.close();
+    }
   }
-}
 
-setupIndexes();
+  setupIndexes();
